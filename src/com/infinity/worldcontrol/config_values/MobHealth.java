e32fd6df6661d71248bf98e48c1b9742f9873f70
xXInfinityXxDEV <joshua.sheldon@cfl.rr.com>
2015-08-22 19:06:41 -0400

    Period Position Change
    In Config Values, the period was after the values, not before them,
    which may have been the cause for the MobHealth feature to not work.

--- src/com/infinity/worldcontrol/config_values/MobHealth.java
+++ src/com/infinity/worldcontrol/config_values/MobHealth.java
@@ -13,41 +13,41 @@ public class MobHealth implements Listener{
 	WC wc = WC.instance;
 	
 	//Peaceful Mobs
-	public int batHealth = wc.getConfig().getInt("MobHealth" + "BAT.");
-	public int chickenHealth = wc.getConfig().getInt("MobHealth" + "CHICKEN.");
-	public int cowHealth = wc.getConfig().getInt("MobHealth" + "COW.");
-	public int pigHealth = wc.getConfig().getInt("MobHealth" + "PIG.");
-	public int rabbitHealth = wc.getConfig().getInt("MobHealth" + "RABBIT.");
-	public int sheepHealth = wc.getConfig().getInt("MobHealth" + "SHEEP.");
-	public int squidHealth = wc.getConfig().getInt("MobHealth" + "SQUID.");
-	public int villagerHealth = wc.getConfig().getInt("MobHealth" + "VILLAGER.");
+	public int batHealth = wc.getConfig().getInt("MobHealth" + ".BAT");
+	public int chickenHealth = wc.getConfig().getInt("MobHealth" + ".CHICKEN");
+	public int cowHealth = wc.getConfig().getInt("MobHealth" + ".COW");
+	public int pigHealth = wc.getConfig().getInt("MobHealth" + ".PIG");
+	public int rabbitHealth = wc.getConfig().getInt("MobHealth" + ".RABBIT");
+	public int sheepHealth = wc.getConfig().getInt("MobHealth" + ".SHEEP");
+	public int squidHealth = wc.getConfig().getInt("MobHealth" + ".SQUID");
+	public int villagerHealth = wc.getConfig().getInt("MobHealth" + ".VILLAGER");
 	
 	//Neutral Mobs
-	public int spiderHealth = wc.getConfig().getInt("MobHealth" + "SPIDER.");
-	public int endermanHealth = wc.getConfig().getInt("MobHealth" + "ENDERMAN.");
-	public int zpmHealth = wc.getConfig().getInt("MobHealth" + "ZOMBIEPIGMAN.");
+	public int spiderHealth = wc.getConfig().getInt("MobHealth" + ".SPIDER");
+	public int endermanHealth = wc.getConfig().getInt("MobHealth" + ".ENDERMAN");
+	public int zpmHealth = wc.getConfig().getInt("MobHealth" + ".ZOMBIEPIGMAN");
 	
 	//Hostile Mobs
-	public int blazeHealth = wc.getConfig().getInt("MobHealth" + "BLAZE.");
-	public int creeperHealth = wc.getConfig().getInt("MobHealth" + "CREEPER.");
-	public int guardHealth = wc.getConfig().getInt("MobHealth" + "GUARDIAN.");
-	public int emiteHealth = wc.getConfig().getInt("MobHealth" + "ENDERMITE.");
-	public int ghastHealth = wc.getConfig().getInt("MobHealth" + "GHAST.");
-	public int sfishHealth = wc.getConfig().getInt("MobHealth" + "SILVERFISH.");
-	public int skellyHealth = wc.getConfig().getInt("MobHealth" + "SKELETON.");
-	public int witchHealth = wc.getConfig().getInt("MobHealth" + "WITCH.");
-	public int zombieHealth = wc.getConfig().getInt("MobHealth" + "ZOMBIE.");
+	public int blazeHealth = wc.getConfig().getInt("MobHealth" + ".BLAZE");
+	public int creeperHealth = wc.getConfig().getInt("MobHealth" + ".CREEPER");
+	public int guardHealth = wc.getConfig().getInt("MobHealth" + ".GUARDIAN");
+	public int emiteHealth = wc.getConfig().getInt("MobHealth" + ".ENDERMITE");
+	public int ghastHealth = wc.getConfig().getInt("MobHealth" + ".GHAST");
+	public int sfishHealth = wc.getConfig().getInt("MobHealth" + ".SILVERFISH");
+	public int skellyHealth = wc.getConfig().getInt("MobHealth" + ".SKELETON");
+	public int witchHealth = wc.getConfig().getInt("MobHealth" + ".WITCH");
+	public int zombieHealth = wc.getConfig().getInt("MobHealth" + ".ZOMBIE");
 	
 	//Tamable Mobs
-	public int ocelotHealth = wc.getConfig().getInt("MobHealth" + "OCELOT.");
+	public int ocelotHealth = wc.getConfig().getInt("MobHealth" + ".OCELOT");
 	
 	//Utility Mobs
-	public int irongHealth = wc.getConfig().getInt("MobHealth" + "IRONGOLEM.");
-	public int snowgHealth = wc.getConfig().getInt("MobHealth" + "SNOWGOLEM.");
+	public int irongHealth = wc.getConfig().getInt("MobHealth" + ".IRONGOLEM");
+	public int snowgHealth = wc.getConfig().getInt("MobHealth" + ".SNOWGOLEM");
 	
 	//Boss Mobs
-	public int dragonHealth = wc.getConfig().getInt("MobHealth" + "ENDERDRAGON.");
-	public int witherHealth = wc.getConfig().getInt("MobHealth" + "WITHER.");
+	public int dragonHealth = wc.getConfig().getInt("MobHealth" + ".ENDERDRAGON");
+	public int witherHealth = wc.getConfig().getInt("MobHealth" + ".WITHER");
 	
 	@EventHandler
 	public void onMobSpawn(CreatureSpawnEvent e){
